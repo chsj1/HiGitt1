d0790b9f0fdb884deb976a0a85194a022334bf90
chsj2 <1019648568@qq.com>
2014-10-14 11:04:55 +0800

    asdasdasdasdasdasdasda

--- src/HiGitt1.java
+++ src/HiGitt1.java
@@ -2,5 +2,7 @@
 public class HiGitt1 {
 	public static void main(String[] args) {
 		System.out.println("------------->hello world");
+		
+		System.out.println("---------->hahah it seem that ia haved succeded");
 	}
 }
